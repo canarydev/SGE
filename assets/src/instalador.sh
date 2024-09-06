6af30136480732ed105b6dd974c5cafc429a43ad
Joshua González Santana <canary_styl3@hotmail.com>
2024-09-06 14:02:36 +0200

    Update instalador.sh

--- assets/src/instalador.sh
+++ assets/src/instalador.sh
@@ -5,6 +5,7 @@
         echo "Este script debe ejecutarse como root. Por favor, utiliza 'sudo' o inicia sesión como root."
         exit 1
         fi
+        
 # Instalar guest additions
         /media/$SUDO_USER/VBox_GAs_7.0.12/VBoxLinuxAdditions.run
 
