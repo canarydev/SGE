07c8635fdb5b2c72c5c6d944647dc198727b46f9
Joshua González Santana <canary_styl3@hotmail.com>
2024-09-09 10:38:10 +0200

    Update instalador.sh

--- assets/src/instalador.sh
+++ assets/src/instalador.sh
@@ -128,12 +128,12 @@
         docker run -d -v odoo:/var/lib/odoo -v extra-addons:/mnt/extra-addons -p 8069:8069 --name odoo --link db:db -t odoo:$odoo_version
 
 # Instalar VScode
-        apt install wget gpg
+        apt install wget gpg -y
         wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
         install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
         echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
         rm -f packages.microsoft.gpg
-        apt install apt-transport-https
+        apt install apt-transport-https -y
         apt update
         apt install code
 
