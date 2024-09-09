a979ba7f406fc261edaecba2adc9b6ad86f36c1d
Joshua González Santana <canary_styl3@hotmail.com>
2024-09-09 11:56:17 +0200

    Update instalador.sh

--- assets/src/instalador.sh
+++ assets/src/instalador.sh
@@ -45,7 +45,7 @@
 # Instalar portainer
         docker volume create portainer_data
         docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-        sleep 1
+        sleep 4
         docker restart portainer
 
 # Instalar postgresql
