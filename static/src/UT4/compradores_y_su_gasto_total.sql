098b78357f7c2d8973faf7033ae7c47bf6d5308d
Joshua González Santana <canary_styl3@hotmail.com>
2024-10-14 20:09:10 +0200

    Update compradores_y_su_gasto_total.sql
    más sencilla

--- static/src/UT4/compradores_y_su_gasto_total.sql
+++ static/src/UT4/compradores_y_su_gasto_total.sql
@@ -1,10 +1,4 @@
-WITH totales as (
-	SELECT po.partner_id, SUM(amount_total) as total
-	FROM POS_order AS po
-	WHERE po.partner_id IS NOT NULL
-	GROUP BY po.partner_id
-	ORDER BY total DESC)
-
-SELECT name, total
-FROM res_partner
-JOIN totales ON totales.partner_id = res_partner.id;
+SELECT p.name, SUM(po.amount_total)
+FROM res_partner p
+JOIN pos_order po ON p.id = po.partner_id
+GROUP BY p.name;
