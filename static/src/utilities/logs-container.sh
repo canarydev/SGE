53b89e90feca5449640c632b5178fe1766ec1acc
Joshua González Santana <canary_styl3@hotmail.com>
2024-11-26 16:29:54 +0100

    Update logs-container.sh

--- static/src/utilities/logs-container.sh
+++ static/src/utilities/logs-container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "\$#" -ne 1 ]; then
+if [ "$#" -ne 1 ]; then
     echo "Usage: logs-container <container-name>"
     exit 1
 fi
 
-CONTAINER_NAME="\$1"
+CONTAINER_NAME="$1"
 docker logs -f "$CONTAINER_NAME"
